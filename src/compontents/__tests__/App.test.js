bb903944b039f099d6a26907cd2583d8976c9d87
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-30 18:46:53 +0100

    test

--- src/compontents/__tests__/App.test.js
+++ src/compontents/__tests__/App.test.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+it('shows a comment box', () => {
+    const div = document.create('div');
+
+    ReactDOM.render(<App />, div)
+
+    ReactDOM.unmountComponentAtNode(div)
+});
+
+it('shows a comment list', () => {
+
+})
