d14aefd3bdff77f45e2a6e2b1aac36e747d0a930
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 10:50:04 +0100

    fetch from api and starting working on integration tests

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from '../Root';
+import App from '../compontents/App';
+
+it('can fetch a list of comments and display them', () => {
+    //attemntp to render the entire app
+
+    // find the fetchComments buttons and click it
+
+    //expect to find a list of comments
+})
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { SAVE_COMMENT } from './types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from './types';
+import axios from 'axios';
 
 export function saveComment(comment) {
     return {
@@ -6,3 +7,12 @@ export function saveComment(comment) {
         payload: comment
     };
 }
+
+export function fetchComments() {
+    const response = axios.get('http://jsonplaceholder.typicode.com/comments');
+
+    return {
+        type: FETCH_COMMENTS,
+        payload: response
+    }
+};
