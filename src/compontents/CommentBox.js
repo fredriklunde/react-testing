72860e4a368aa65e1035817498c3e7211ec6b61b
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 23:39:01 +0100

    protect route from unauthorized users

--- src/compontents/CommentBox.js
+++ src/compontents/CommentBox.js
@@ -4,6 +4,20 @@ import * as actions from '../actions';
 
 class CommentBox extends React.Component {
     state = {comment: ''};
+    
+    componentDidMount() {
+        this.shouldNavigateAway();
+    }
+
+    componentDidUpdate() {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+        if(!this.props.auth){
+            this.props.history.push("/");
+        }
+    }
 
     handleChange = (event) => {
         this.setState({comment: event.target.value })
@@ -33,4 +47,10 @@ class CommentBox extends React.Component {
     }
 }
 
-export default connect(null, actions)(CommentBox);
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    };
+}
+
+export default connect(mapStateToProps, actions)(CommentBox);
