4c593fc4c92bf14fd95cce3b1d9c40094cc9b64e
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 11:16:41 +0100

    moxios

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from '../Root';
 import App from '../compontents/App';
+
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [
+            {name: 'Fetched #1'},
+            {name: 'Fetched #2'}
+        ]
+    });
+});
+
+afterEach(() => {
+    moxios.uninstall();
+})
 
 it('can fetch a list of comments and display them', () => {
     //attemntp to render the entire app
@@ -14,5 +30,5 @@ it('can fetch a list of comments and display them', () => {
     wrapped.find('.fetch-comments').simulate('click');
 
     //expect to find a list of comments
-    expect(wrapped.find('li').length).toEqual(500);
+    expect(wrapped.find('li').length).toEqual(2);
 })
