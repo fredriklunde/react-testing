6f2d85539ca1c6b6b2d1bf3ad75abc2895d68a3f
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 23:41:51 +0100

    scafolding higher order component

---
+++ src/compontents/requireAuth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default (ChildComponent) => {
+    class ComposedComponent extends React.Component {
+        render() {
+            return (
+                <ChildComponent />
+            );
+        }
+    }
+
+    return ComposedComponent;
+}
