5e5ddea655fd9f80de87ae04594e7bc60765865e
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 23:16:51 +0100

    auth reducer and action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from './types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from './types';
 import axios from 'axios';
 
 export function saveComment(comment) {
@@ -16,3 +16,10 @@ export function fetchComments() {
         payload: response
     }
 };
+
+export function changeAuth(isLoggedIn) {
+    return {
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
+    }
+}
