98b4ed0ebd56c96c793da2bb09e13034da6f170a
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-04 00:52:40 +0100

    middelware redux-promise

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
 import reducers from './reducers'
+import async from './middlewares/async'
 
 export default ({ children, initialState = {}}) => {
     const store = createStore(
         reducers, 
         initialState, 
-        applyMiddleware(reduxPromise)
+        applyMiddleware(async)
     );
     
     return (
