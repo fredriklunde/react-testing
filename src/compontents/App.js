b6b37f31eee1cac72d9f660b34adf2488a20da1d
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 23:25:32 +0100

    Add Header

--- src/compontents/App.js
+++ src/compontents/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-export default () => {
-    return (
-        <div>
-            <Route path="/post" component={CommentBox} />
-            <Route path="/" exact component={CommentList} />
-        </div>
-    )
+class App extends React.Component {
+    renderButton() {
+        if(this.props.auth){
+            return(
+                <button>
+                    Sign Out
+                </button>
+            );
+        }
+        return(
+            <button>
+                Sign in
+            </button>
+        )
+    }
+    renderHeader() {
+        return (
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/post">Post a comment</Link>
+                </li>
+                <li>
+                    {this.renderButton()}
+                </li>
+            </ul>
+        )
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderHeader()}
+                <Route path="/post" component={CommentBox} />
+                <Route path="/" exact component={CommentList} />
+            </div>
+        )
+    }
 }
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(App);
