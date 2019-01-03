2aaea5231b708fb2f1a65785a2d3f7520e901d31
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-03 11:04:10 +0100

    adding classname to button in order to find in test

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -5,8 +5,14 @@ import App from '../compontents/App';
 
 it('can fetch a list of comments and display them', () => {
     //attemntp to render the entire app
-
+    const wrapped = mount(
+        <Root>
+            <App />
+        </Root>
+    );
     // find the fetchComments buttons and click it
+    wrapped.find('.fetch-comments').simulate('click');
 
     //expect to find a list of comments
+    expect(wrapped.find('li').length).toEqual(500);
 })
