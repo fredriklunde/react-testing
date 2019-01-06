7c8ffbaea1cb78d92d93a9bd83ffb2782433a8af
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-06 10:45:16 +0100

    Add Json schema validation middleware

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers'
+import stateValidator from './middlewares/stateValidator';
 import async from './middlewares/async'
 
 export default ({ children, initialState = {}}) => {
     const store = createStore(
         reducers, 
         initialState, 
-        applyMiddleware(async)
+        applyMiddleware(async, stateValidator)
     );
     
     return (
